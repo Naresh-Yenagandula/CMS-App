a41a86c048d04f2ce68ef8950491f642d37e9199
unknown <nareshyenagandula98@gmail.com>
2021-04-10 09:07:29 +0530

    navbar and routing done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router';
+
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {PagesComponent} from './pages/pages.component';
+import {CategoriesComponent} from './categories/categories.component';
+import {UsersComponent} from './users/users.component';
+import {LoginComponent} from './login/login.component';
+
+const routes:Routes=[
+  {path:'',redirectTo:'dashboard',pathMatch:'full'},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'pages',component:PagesComponent},
+  {path:'categories',component:CategoriesComponent},
+  {path:'users',component:UsersComponent},
+  {path:'login',component:LoginComponent}
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    PagesComponent,
+    CategoriesComponent,
+    UsersComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

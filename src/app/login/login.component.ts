5d116b961ae300efa02fc0a2fcdb1ffdda3ec443
Ratnesh Mishra <mishra48.ratnesh@gmail.com>
2021-04-13 14:33:05 +0530

    logService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute,Router} from '@angular/router';
-
+import {AuthLoginService} from '../auth-login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,11 +10,21 @@ import {ActivatedRoute,Router} from '@angular/router';
 export class LoginComponent implements OnInit {
   loginForm:FormGroup;
   hide=true;
-  constructor(private fb:FormBuilder,private router:Router,private route:ActivatedRoute) { }
+  constructor(private fb:FormBuilder,private router:Router,private route:ActivatedRoute,private authService:AuthLoginService) { }
 
   onSubmit():void{
-    console.log(this.loginForm.value);
-    this.router.navigate([''])
+   /*  console.log(this.loginForm.value);
+    this.router.navigate(['']) */
+    if(this.loginForm.valid){
+      this.authService.loginUser(this.loginForm.value).subscribe(result=>{
+        if(result.success){
+          console.log(result);
+          alert(result.message);  
+        }else{
+          alert(result.message);
+        }
+      });
+    }
     
   }
 
